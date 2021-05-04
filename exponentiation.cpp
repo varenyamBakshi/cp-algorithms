bd2b19396a0c93ea2811eaed2602f0c13aee781b
varenyamBakshi <varenyam101@gmail.com>
2021-05-05 03:49:35 +0530

    adding comments

--- exponentiation.cpp
+++ exponentiation.cpp
@@ -1,13 +1,16 @@
+// We are implementing the power function to calculate the x raised to y in log y time complexity.
+// It is binary exponentiation.
+
 #include<bits/stdc++.h>
 using namespace std;
 
 #define ll long long
-const int mod = 1e9+7;
+const int mod = 1e9+7;// for large results ometimes we are asked to return modulo of reult
 
 ll exp_pow(ll x, ll y){ // returns x to power y
     ll res = 1;
     while(y>0){
-        if(y&1) res *= x; // if lsb is 1 we multiply the calculates power of x to the result 
+        if(y&1) res *= x; // if lsb is 1 we multiply the calculated power of x to the result 
         x *= x;
         y>>=1;
     }
