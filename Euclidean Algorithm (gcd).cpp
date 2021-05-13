1ba2a4eb1e79803ed747a6cd6e9216bfdc3c234a
varenyamBakshi <varenyam101@gmail.com>
2021-05-14 04:35:44 +0530

    renamed gcd.cpp

--- Euclidean Algorithm (gcd).cpp
+++ Euclidean Algorithm (gcd).cpp
@@ -1,4 +1,4 @@
-// We implement the gcd function using the Euler formula. If we have to find the gcd of a and b then time complexity would be 
+// We implement the gcd function using the Euclidean formula. If we have to find the gcd of a and b then time complexity would be 
 // O(log(min(a,b))). Also if one of the number is zero the gcd is defined as the other number. Thus the algorithm works for
 // non negative numbers.
 
