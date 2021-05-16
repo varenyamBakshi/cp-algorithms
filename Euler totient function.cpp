693e7a2b69355172424ac5a96772053f299546fd
varenyamBakshi <varenyam101@gmail.com>
2021-05-16 16:35:24 +0530

    Euler totient function in sqrt(n)

--- Euler totient function.cpp
+++ Euler totient function.cpp
@@ -3,14 +3,13 @@ using namespace std;
 
 int etf(int n){
     int res = n;
-    vector<bool>primes(n+1,true);
-    primes[0] = primes[1] = false;
-    for(int i = 2; i <=n; i++){
-        if(primes[i]){
-            for(int j = i*i; j <=n; j+=i)primes[j] = false;
-            if(n%i == 0) res = res/i*(i-1);
+    for(int i = 2; i*i <= n; i++){
+        if(n%i==0){
+            res = res/i*(i-1);
+            while(n%i==0)n /= i;
         }
     }
+    if(n>1) res = res/n*(n-1);
     return res;
 }
 
